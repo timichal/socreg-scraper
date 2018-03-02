e26eb5b2b73596417a769b11251653f25f610aa3
timichal <michal@zlatkovsky.cz>
2018-03-02 15:20:25 +0100

    refining

--- scraper.py
+++ scraper.py
@@ -1,13 +1,31 @@
-import requests 
+import csv, requests 
 
-with open("id_list") as file:
-    with open("mail_list", "w") as outfile:
-        for line in file:
-            inst_id = line.strip()
-            email = "(bez emailu)"
-            link = "https://iregistr.mpsv.cz/socreg/rozsirene_hledani_sluzby.do?si=" + inst_id
-            content = requests.get(link).text.split()
-            for line in content:
-                if "mailto" in line:
-                    email = line.split("\"")[1][7:]
-            outfile.write(inst_id + " " +  email + "\n")
+with open("PSSvsichni.tsv","r") as file, open("PSSemaily.tsv", "w", newline="") as outfile:
+    tsvlines = []
+    inst_ids = set()
+    maildict = {}
+
+    tsvin = csv.reader(file, delimiter="\t")
+    for row in tsvin:
+        inst_ids.add(row[1])
+        tsvlines.append(row)
+
+    for counter,inst_id in enumerate(inst_ids):
+        email = "(bez emailu)"
+        link = "https://iregistr.mpsv.cz/socreg/rozsirene_hledani_sluzby.do?si=" + inst_id
+        content = requests.get(link).text.split()
+        for line in content:
+            if "mailto" in line:
+                email = line.split("\"")[1][7:]
+        print(str(counter+1) + "/" + str(len(inst_ids)) + ": " + inst_id + " " + email)
+        maildict[inst_id] = email
+    print(maildict)
+
+    for line in tsvlines:
+        if line[1] in maildict:
+            line.append(maildict[line[1]])
+    
+    tsvout = csv.writer(outfile, delimiter="\t")
+
+    for line in tsvlines:
+        tsvout.writerow(line)
